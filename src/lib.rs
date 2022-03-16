abae89cfa9e62f8186ee7fc2555d4471c32f488d
Raphael Darley <raphael@raphaeldarley.com>
2022-03-16 20:13:30 +0000

    basic point rendering working

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+mod donut_utils;
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,106 @@
+use donut_utils::*;
+
 fn main() {
-    // let point1 = Point{x: 0.0, y:5.0, z: 10.0,};
-    // println!("{:?} projects to pixel {:?}", point1, point1.project(2.0));
-    // draw(vec![point1.project(2.0)])
-    draw(vec![Point{x: 0.0, y:5.0, z: 10.0,}], 2.0);
+    draw(
+        vec![Point {
+            x: 0.0,
+            y: 5.0,
+            z: 10.0,
+        }],
+        CameraConfig::new(2.0, 20, 10, 10.0, 10.0),
+    );
 }
 
+fn draw(scene: Vec<Point>, config: CameraConfig) {
+    let screen = scene
+        .into_iter()
+        .map(|point| point.project(config.screen_dist));
 
-fn draw(scene: Vec<Point>, screen_dist: f32) {
+    // let x_resolution = 20;
+    // let y_resolution = 10;
+    // let x_fov = 10.0;
+    // let y_fov = 10.0;
+    // let x_step = x_fov / x_resolution as f32;
+    // let y_step = y_fov / y_resolution as f32;
 
-    let screen = scene.into_iter().map(|point|{point.project(screen_dist)});
-
-    let x_resolution = 20;
-    let y_resolution = 10;
-    let x_fov = 10.0;
-    let y_fov = 10.0;
-    let x_step = x_fov/x_resolution as f32;
-    let y_step = y_fov/y_resolution as f32;
-
-    let mut disp_acc = vec![vec![0;x_resolution];y_resolution];
-    
-    // println!("{:?}", disp_acc);
-    
-    println!("{:?}", screen);
-
-    // println!("len: {}", screen.len());
+    let mut disp_acc = vec![vec![0; config.x_res]; config.y_res];
 
     for pxl in screen {
-        if pxl.x.abs() >= x_fov/2.0 {continue;}
-        if pxl.y.abs() >= y_fov/2.0 {continue;}
+        if pxl.x.abs() >= config.x_fov / 2.0 {
+            continue;
+        }
+        if pxl.y.abs() >= config.y_fov / 2.0 {
+            continue;
+        }
 
-        println!("x pos from right:{}", (pxl.x + x_fov/2.0)       );
-        println!("y pos from top:{}", (y_fov/2.0 - pxl.y)       );
+        println!("x pos from right:{}", (pxl.x + config.x_fov / 2.0));
+        println!("y pos from top:{}", (config.y_fov / 2.0 - pxl.y));
 
-        disp_acc[((y_fov/2.0 - pxl.y) /y_step).floor() as usize][((pxl.x + x_fov/2.0) /x_step).floor() as usize] +=1;
-
+        disp_acc[((config.y_fov / 2.0 - pxl.y) / config.y_step).floor() as usize]
+            [((pxl.x + config.x_fov / 2.0) / config.x_step).floor() as usize] += 1;
     }
 
-    // println!("{:?}", disp_acc);
+    for row in disp_acc {
+        for p in row {
+            print!("{}", p)
+        }
 
-    for row in disp_acc{
-        for p in row{print!("{}", p)}
         print!("\n")
     }
-    
 }
 
+mod donut_utils {
 
+    pub struct CameraConfig {
+        pub screen_dist: f32,
+        pub x_res: usize,
+        pub y_res: usize,
+        pub x_fov: f32,
+        pub y_fov: f32,
+        pub x_step: f32,
+        pub y_step: f32,
+    }
 
-#[derive(Debug)]
-struct Pixel {
-    x: f32,
-    y: f32,
-}
+    impl CameraConfig {
+        pub fn new(
+            screen_dist: f32,
+            x_res: usize,
+            y_res: usize,
+            x_fov: f32,
+            y_fov: f32,
+        ) -> CameraConfig {
+            CameraConfig {
+                screen_dist,
+                x_res,
+                y_res,
+                x_fov,
+                y_fov,
+                x_step: x_fov / x_res as f32,
+                y_step: y_fov / y_res as f32,
+            }
+        }
+    }
 
-#[derive(Debug)]
-struct Point{
-    x: f32,
-    y: f32,
-    z: f32,
-}
+    #[derive(Debug)]
+    pub struct Projection {
+        pub x: f32,
+        pub y: f32,
+    }
 
-impl Point {
-    fn project(&self, screen_dist: f32) -> Pixel {
-        let scale = screen_dist / self.z;
-        Pixel{x: self.x * scale, y: self.y * scale}
+    #[derive(Debug)]
+    pub struct Point {
+        pub x: f32,
+        pub y: f32,
+        pub z: f32,
+    }
+
+    impl Point {
+        pub fn project(&self, screen_dist: f32) -> Projection {
+            let scale = screen_dist / self.z;
+            Projection {
+                x: self.x * scale,
+                y: self.y * scale,
+            }
+        }
     }
 }
